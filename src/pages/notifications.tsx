bc5864a5a5fadc7f20a009f708b51a49cb98849b
Antonny Mello <antonny.mello@gmail.com>
2021-09-19 13:28:23 -0300

    Removing unused code

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -1,9 +1,6 @@
 import Layout from '../components/template/Layout';
-import useAppData from '../data/hook/useAppData';
 
 const Notifications = () => {
-  const { changeTheme } = useAppData();
-
   return (
     <Layout title='Notificações' subtitle='Manage your notifications'>
       <h1>Content</h1>
